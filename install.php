730737b3d080d8f96728428aa3d586ab837b0ac2
Valentin CARRUESCO <idleman@idleman.fr>
2013-02-22 09:03:05 +0000

    Libération de la clause ND de la licence créative common, vous  avez maintenant le droit de modifier, de transformer ou d'adapter leed. Je vous fait confiance, ne faites pas de bêtise (c'est un peu mon bébé leed :p)
    git-svn-id: http://projet.idleman.fr/leed.svn@87 cbb609ad-8cd9-463b-aa97-3ec7c4f0f680

--- install.php
+++ install.php
@@ -64,7 +64,7 @@ if(isset($_['installButton'])){
 
 	$constant = "<?php
 	define('VERSION_NUMBER','1.1');
-	define('VERSION_NAME','Beta (rev 86)');
+	define('VERSION_NAME','Beta (rev 87)');
 
 	//Host de Mysql, le plus souvent localhost ou 127.0.0.1
 	define('MYSQL_HOST','".$_['mysqlHost']."'); 
