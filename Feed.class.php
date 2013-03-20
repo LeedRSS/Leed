8084ae9d4fae386de2e606064f37a53e091689af
Idleman <idleman@idleman.fr>
2013-03-20 12:03:18 +0100

    Ajout d'un oubli de div sur les ecnlosure (merci à Simon)

--- Feed.class.php
+++ Feed.class.php
@@ -88,7 +88,7 @@ class Feed extends MysqlEntity{
 						if($enclosureArgs!==false) $enclosureName = substr($enclosureName,0,$enclosureArgs);
 						$enclosureFormat = (isset($enclosure->handler)?$enclosure->handler:substr($enclosureName,strrpos($enclosureName,'.')+1));
 					
-						$enclosure ='<div class="enclosure"><h1>Fichier média :</h1><a href="'.$enclosure->link.'"> '.$enclosureName.'</a> <span>(Format '.strtoupper($enclosureFormat).', '.Functions::convertFileSize($enclosure->length).')</span>';
+						$enclosure ='<div class="enclosure"><h1>Fichier média :</h1><a href="'.$enclosure->link.'"> '.$enclosureName.'</a> <span>(Format '.strtoupper($enclosureFormat).', '.Functions::convertFileSize($enclosure->length).')</span></div>';
 					}else{
 						$enclosure = '';
 					}
