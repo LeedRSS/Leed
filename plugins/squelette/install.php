157b0116b33e1d63c2edfa05106b7ef5d52f207b
Idleman <idleman@idleman.fr>
2013-03-25 11:25:05 +0100

    Ajout d'un plugin squelette d'exemple

---
+++ plugins/squelette/install.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+	Le code contenu dans cette page ne sera éxecuté qu'à l'activation du plugin 
+	Vous pouvez donc l'utiliser pour créer des tables MySQL, des dossiers, ou executer une action
+	qui ne doit se lancer qu'à l'installation ex :
+	
+	mysql_query('
+		CREATE TABLE IF NOT EXISTS `'.MYSQL_PREFIX.'plugin_squelette` (
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `squelette_champ1` int(11) NOT NULL,
+		  PRIMARY KEY (`id`)
+		) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	');
+
+*/
+?>
---
+++ plugins/squelette/uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+	Le code contenu dans cette page ne sera éxecuté qu'à la désactivation du plugin 
+	Vous pouvez donc l'utiliser pour supprimer des tables MySQL, des dossiers, ou executer une action
+	qui ne doit se lancer qu'à la désinstallation ex :
+	
+	mysql_query('DROP TABLE '.MYSQL_PREFIX.'plugin_squelette');
+
+*/
+?>
