f084f152fa9e27afc8b17e3501b9fc29a0f8b4ad
Idleman <idleman@idleman.fr>
2013-03-28 02:02:35 +0100

    Supression des plugins de la branche master, tous les plugins doivent maintenant être placé dans le dépot https://github.com/ldleman/Leed-market/
    N'hésitez pas à y faire vos pull request pour ajouter/modifier vos plugins

--- plugins/social/js/main.js
+++
@@ -1,4 +0,0 @@
-function openURL(url){
-   window.open(url, '_blank');
-   return false;
-}
--- plugins/squelette/js/main.js
+++
@@ -1,5 +0,0 @@
-//fonction appelée depuis la fonction squelette_plugin_menu de la page squelette.plugin.[enabled/disabled].php 
-function squelette_javascript(){
-	alert('Cette fonction ne sert à rien :p');
-}
-
