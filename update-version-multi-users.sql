94cd4ae83d120316aa769ab40b1cf8be12b0bfb4
cobalt74 <cobalt74@gmail.com>
2013-06-09 14:25:34 +0200

    #1 - mai requête pour passer d'une version mono a multi

--- update-version-multi-users.sql
+++ update-version-multi-users.sql
@@ -10,7 +10,12 @@ Description :
 - utilisé pour un passage de la version mono a multi utilisateur.
 - Les requêtes suivantes sont a executer sur votre Base de données Leed avec phpMyAdmin par exemple
 
+
+ATTENTION, si votre prefix a été changé lors de votre première installation, il faut le remplacer.
 ############### */
 
 -- Mise à jour table User pour le prefix des Tables en fonction des utilisateurs
 ALTER TABLE `leed_user` ADD `prefixDatabase` VARCHAR(255) NOT NULL;
+
+-- mise à jour de l'utilisateur admin - affectation du prefix de base.
+UPDATE `leed_user` set `prefixDatabase` = 'leed_' where `id` = 1;
