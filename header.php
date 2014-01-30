8e5488bc8be05f1e4d2ed47eb57ec8272cc30a69
Christophe HENRY <christophe.henry@sbgodin.fr>
2014-01-30 06:00:26 +0100

    Enlève l'erreur à la suppression de constant.php
    En cours de développement, lorsqu'on supprime le fichier "constant.php",
    ça vérifie si ce fichier existe, et redirige vers l'installation.
    
    L'en-tête de redirection était envoyée, mais le script invoquait quand
    même "common.php" et générait l'erreur. Le script s'arrête maintenant
    après l'envoi de l'en-tête.

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <?php
-if(!file_exists('constant.php'))header('location: install.php');
+if(!file_exists('constant.php')) {
+    header('location: install.php');
+    exit();
+}
 require_once('common.php');
 
 ?>
