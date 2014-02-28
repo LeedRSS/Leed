5c9663db016e5dee55f0d4ca898672ab0eb80e8f
Christophe HENRY <christophe.henry@sbgodin.fr>
2014-02-28 16:17:53 +0100

    Fait disparaître l'événement lu avant le XHR
    Le clic sur "Lu/Non lu" fait maintenant tout de suite disparaître
    l'événement plutôt que d'attendre la fin de la requête réseau.
    
    Il peut y avoir des cas où la connexion n'est pas faite, impliquant
    que l'événement disparaît à l'écran mais réapparaîtra par la suite.
    Hormis cela, la réactivité est immédiate quand on est sur une connexion
    lente.

--- templates/marigolds/js/script.js
+++ templates/marigolds/js/script.js
@@ -421,6 +421,24 @@ function readThis(element,id,from,callback){
     var nextEvent = $('#'+id).next();
     //sur les éléments non lus
     if(!parent.hasClass('eventRead')){
+        switch (activeScreen){
+            case '':
+                // cas de la page d'accueil
+                parent.addClass('eventRead');
+                parent.fadeOut(200,function(){
+                    if(callback){
+                        callback();
+                    }else{
+                        targetThisEvent(nextEvent,true);
+                    }
+                    // on simule un scroll si tous les events sont cachés
+                    if($('article section:last').attr('style')=='display: none;') {
+                        $(window).scrollTop($(document).height());
+                    }
+                });
+                // on diminue le nombre d'article en haut de page
+                $('#nbarticle').html(parseInt($('#nbarticle').html()) - 1)
+        }
         $.ajax({
             url: "./action.php?action=readContent",
             data:{id:id},
@@ -431,23 +449,8 @@ function readThis(element,id,from,callback){
                     if( console && console.log && msg!="" ) console.log(msg);
                     switch (activeScreen){
                         case '':
-                            // cas de la page d'accueil
-                            parent.addClass('eventRead');
-                            parent.fadeOut(200,function(){
-                                if(callback){
-                                    callback();
-                                }else{
-                                    targetThisEvent(nextEvent,true);
-                                }
-                                // on simule un scroll si tous les events sont cachés
-                                if($('article section:last').attr('style')=='display: none;') {
-                                    $(window).scrollTop($(document).height());
-                                }
-                            });
                             // on compte combien d'article ont été lus afin de les soustraires de la requête pour le scroll infini
                             $(window).data('nblus', $(window).data('nblus')+1);
-                            // on diminue le nombre d'article en haut de page
-                            $('#nbarticle').html(parseInt($('#nbarticle').html()) - 1)
                         break;
                         case 'selectedFolder':
                         case 'selectedFeedNonLu':
