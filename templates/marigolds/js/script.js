aec79cbc0a7477a173b11663e668033b07bfec48
Christophe HENRY <christophe.henry@sbgodin.fr>
2014-03-01 12:57:03 +0100

    Recopie le reste des instructions

--- templates/marigolds/js/script.js
+++ templates/marigolds/js/script.js
@@ -435,6 +435,10 @@ function readThis(element,id,from,callback){
                         $(window).scrollTop($(document).height());
                     }
                 });
+                // on compte combien d'article ont été lus afin de les soustraires de la requête pour le scroll infini
+                $(window).data('nblus', $(window).data('nblus')+1);
+                // on diminue le nombre d'article en haut de page
+                $('#nbarticle').html(parseInt($('#nbarticle').html()) - 1)
         }
         $.ajax({
             url: "./action.php?action=readContent",
@@ -446,10 +450,6 @@ function readThis(element,id,from,callback){
                     if( console && console.log && msg!="" ) console.log(msg);
                     switch (activeScreen){
                         case '':
-                            // on compte combien d'article ont été lus afin de les soustraires de la requête pour le scroll infini
-                            $(window).data('nblus', $(window).data('nblus')+1);
-                            // on diminue le nombre d'article en haut de page
-                            $('#nbarticle').html(parseInt($('#nbarticle').html()) - 1)
                         break;
                         case 'selectedFolder':
                         case 'selectedFeedNonLu':
