39b69eb19490f61d5b13ff02f35ffda1b2373251
Simounet <contact@simounet.net>
2016-02-23 22:19:09 +0100

    Filtre sur le dernier évènement sur "marquer comme lu" L'envoi du dernier évènement affiché lors de l'action de "marquer comme lu" permet de ne pas en louper si une synchronisation a été effectuée entre temps.

--- action.php
+++ action.php
@@ -152,6 +152,7 @@ switch ($action){
         $whereClause = array();
         $whereClause['unread'] = '1';
         if(isset($_['feed']))$whereClause['feed'] = $_['feed'];
+        if(isset($_['last-event-id']))$whereClause['id'] = '<= ' . $_['last-event-id'];
         $eventManager->change(array('unread'=>'0'),$whereClause);
         header('location: ./index.php');
     break;
@@ -162,7 +163,9 @@ switch ($action){
         $feeds = $feedManager->loadAllOnlyColumn('id',array('folder'=>$_['folder']));
 
         foreach($feeds as $feed){
-            $eventManager->change(array('unread'=>'0'),array('feed'=>$feed->getId()));
+            $whereClause['feed'] = $feed->getId();
+            if(isset($_['last-event-id']))$whereClause['id'] = '<= ' . $_['last-event-id'];
+            $eventManager->change(array('unread'=>'0'),$whereClause);
         }
 
         header('location: ./index.php');
