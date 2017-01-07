a520906723d6b79e4375a1c4495ccb3f02685814
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-01-07 17:39:44 +0100

    tests

--- otTest.php
+++ otTest.php
@@ -5,8 +5,20 @@
 
 require_once dirname(__FILE__).'/otphp/lib/otphp.php';
 
+
+# Il faut faire comme ça : https://python-totp.herokuapp.com/
+# Ça génère un OTP aléatoire et le QR qui correspond. FreeOTP le récupère
+# d'un coup. Idéal à placer sur la gestion du compte pour activer l'OTP sans se
+# soucier du secret.
+
 $pass = '42432526';
 $pass = 'abcdefgh';
+$pass = 'abcdefgh23456';
+$pass = 'abcdeijrgfoaefgh23456';
+$pass = "yiemah3ShulaeXaichae";
+$pass = "yiemah3shulaexaichae";
+
+$pass = "yiemah3shul";
 $totp1 = new \OTPHP\TOTP($pass, array('interval'=>30, 'digits'=>8, 'digest'=>'sha1'));
 $totp2 = new \OTPHP\TOTP(strtoupper($pass), array('interval'=>30, 'digits'=>8, 'digest'=>'sha1'));
 
