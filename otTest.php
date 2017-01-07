62efb16d36ddff33b0162d38206e9ce1929f12fa
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-01-07 12:51:47 +0100

    tests sur l'otp

--- otTest.php
+++ otTest.php
@@ -5,14 +5,15 @@
 
 require_once dirname(__FILE__).'/otphp/lib/otphp.php';
 
-$totp1 = new \OTPHP\TOTP('22222222', array('interval'=>30, 'digits'=>8, 'digest'=>'sha1'));
-$totp512 = new \OTPHP\TOTP('22222222', array('interval'=>30, 'digits'=>8, 'digest'=>'sha512'));
-print_r($totp);
+$pass = '42432526';
+$pass = 'abcdefgh';
+$totp1 = new \OTPHP\TOTP($pass, array('interval'=>30, 'digits'=>8, 'digest'=>'sha1'));
+$totp2 = new \OTPHP\TOTP(strtoupper($pass), array('interval'=>30, 'digits'=>8, 'digest'=>'sha1'));
 
 
 while( True ){
     echo "1 ", str_pad($totp1->now(), $totp1->digits, '0', STR_PAD_LEFT)."\n";
-    echo "512 ", str_pad($totp512->now(), $totp512->digits, '0', STR_PAD_LEFT)."\n";
+    echo "2 ", str_pad($totp2->now(), $totp2->digits, '0', STR_PAD_LEFT)."\n";
     sleep(1);
 }
 
