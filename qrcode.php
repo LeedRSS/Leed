d942880bffd557748dfd715f839322c9e4881bc5
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-01-15 21:49:36 +0100

    en cours

--- qrcode.php
+++ qrcode.php
@@ -1,11 +1,14 @@
 <?php
 
+
+/*
+ * phpinfo();
+exit();
+ */
 require_once('common.php');
 ##if (empty($myUser)) exit();
 
 require_once('phpqrcode.php');
-
-#$qrcode = $_['q'];
 
 function chargeVarRequest() {
     foreach (func_get_args() as $arg) {
@@ -19,15 +22,22 @@ function chargeVarRequest() {
     }
 }
 
-echo "<pre>";
-
-switch(array_keys($_REQUEST)[0]) {
+$methode = array_keys($_REQUEST)[0];
+switch($methode) {
     case 'qr':
-        chargeVarRequest('label', 'user', 'key', 'url', 'issuer');
-        $qrCode = "otpauth://totp/{$label}:{$user}?secret={$key}&issuer={$issuer}";
+        chargeVarRequest('label', 'user', 'key');
+        $qrCode = "otpauth://totp/{$label}:{$user}?secret={$key}";
+        break;
+    case 'txt':
+        $qrCode = substr($_SERVER['QUERY_STRING'], 1+strlen($methode));
         break;
     default:
-        $qrCode = 'bla';
+        $qrCode = '';
 }
 
-QRcode::png($qrCode);
+chargeVarRequest('_qrSize', '_qrMargin');
+if (empty($_qrSize))   $_qrSize   = 3;
+if (empty($_qrMargin)) $_qrMargin = 4;
+
+// public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=false)
+QRcode::png($qrCode, false, 'QR_LEVEL_H', $_qrSize, $_qrMargin);
