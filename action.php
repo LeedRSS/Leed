7a3cb224a841f9b23ad57f611abbb624ad308bda
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-01-15 13:54:16 +0100

    champ OTP sans mémoire et indication si vide

--- action.php
+++ action.php
@@ -136,8 +136,7 @@ switch ($action){
                     $configurationManager->change(array('value'=>$salt), array('key'=>'cryptographicSalt'));
 
             }
-            #$userManager->change(array('otpSeed'=>$_['otpSeed']),array('id'=>$myUser->getId()));
-            $userManager->change(array('otpSeed'=>"azer"),array('id'=>$myUser->getId()));
+            $userManager->change(array('otpSeed'=>$_['otpSeed']),array('id'=>$myUser->getId()));
 
     header('location: ./settings.php#preferenceBloc');
     break;
