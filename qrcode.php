b85ec918be9641a4978345b8d39489e907374502
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-01-16 18:11:44 +0100

    Optimisation QRcode

--- qrcode.php
+++ qrcode.php
@@ -1,47 +1,28 @@
 <?php
 
-
-/*
- * phpinfo();
-exit();
- */
 require_once('common.php');
-##if (empty($myUser)) exit();
+if (empty($myUser)) exit();
 
 require_once('phpqrcode.php');
 
-function chargeVarRequest() {
-    foreach (func_get_args() as $arg) {
-        global ${$arg};
-        if (array_key_exists($arg, $_REQUEST)) {
-            $valeur = $_REQUEST[$arg];
-        } else {
-            $valeur = '';
-        }
-        ${$arg} = $valeur;
-    }
-}
-
 $methode = array_keys($_REQUEST)[0];
 switch($methode) {
-    case 'qr':
-        chargeVarRequest('label', 'user', 'key', 'issuer', 'algorithm', 'digits', 'period');
+    case 'qr': # qrcode.php?qr&label=A&user=B&key=C
+        Functions::chargeVarRequest('label', 'user', 'key', 'issuer', 'algorithm', 'digits', 'period');
         $qrCode = "otpauth://totp/{$label}:{$user}?secret={$key}";
-        if (!empty($issuer))    $qrCode.="&issuer={$issuer}";
-        if (!empty($algorithm)) $qrCode.="&algorithm={$algorithm}";
-        if (!empty($digits))    $qrCode.="&digits={$digits}";
-        if (!empty($period))    $qrCode.="&period={$period}";
+        foreach (array('issuer', 'algorithm', 'digits', 'period') as $champ)
+            if (!empty(${$champ}))
+                $qrCode.="&{$champ}={${$champ}}";
         break;
-    case 'txt':
+    case 'txt': # qrcode.php?txt&TEXTE
         $qrCode = substr($_SERVER['QUERY_STRING'], 1+strlen($methode));
         break;
     default:
         $qrCode = '';
 }
 
-chargeVarRequest('_qrSize', '_qrMargin');
+Functions::chargeVarRequest('_qrSize', '_qrMargin');
 if (empty($_qrSize))   $_qrSize   = 3;
 if (empty($_qrMargin)) $_qrMargin = 4;
 
-// public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=false)
 QRcode::png($qrCode, false, 'QR_LEVEL_H', $_qrSize, $_qrMargin);
