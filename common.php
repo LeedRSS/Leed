22e0b961a89fc022c994695d9276d9cf6545413f
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-02-09 19:37:09 +0100

    Change le numéro de version : v1.8

--- common.php
+++ common.php
@@ -6,7 +6,7 @@
  @description: Page incluse dans tous (ou presque) les fichiers du projet, inclus les entitées SQL et récupère/traite les variables de requetes
  */
 
-define('VERSION_NUMBER_CODE','1.6.1');
+define('VERSION_NUMBER_CODE','1.8');
 define('VERSION_NAME_CODE','dev');
 
 /* ----------MAJ de la version du constant.php--------------------- */
