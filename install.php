faca78252893579d6c5aff6e7ce763e962302d22
Christophe HENRY <christophe.henry@sbgodin.fr>
2017-02-12 13:35:32 +0100

    Désactive le référent
    Quand on clique sur un lien externe, le site d'arrivée connaît le site
    d'origine via le référent.
    
    Ce correctif désactive la transmission du référent en ajoutant :
    <meta name="referrer" content="no-referrer" />
    
    Source :
    * https://w3c.github.io/webappsec-referrer-policy/#referrer-policy-no-referrer

--- install.php
+++ install.php
@@ -241,6 +241,7 @@ if (isset($_['installButton']) && empty($test[$lib_errors])) { // Pas d'erreur, 
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
+    <meta name="referrer" content="no-referrer" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php echo _t('INSTALL_TITLE') ?></title>
     <meta name="viewport" content="width=device-width">
